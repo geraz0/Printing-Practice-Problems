903763954232e267a09952c0b074ad8c513d10ee
geraz0 <gabbi.erazo1@gmail.com>
2024-07-22 14:59:18 -0400

    uploading My Name Is print

---
+++ src/main/java/org/example/MyNameIs.java
@@ -0,0 +1,15 @@
+package org.example;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class MyNameIs {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.println("My name is");
+
+        System.out.println("Gabriela");
+
+        System.out.println("Erazo");
+    }
+}
