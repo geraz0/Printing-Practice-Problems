6dbc0eabc561bd19d9a93e64064561349500e446
geraz0 <gabbi.erazo1@gmail.com>
2024-07-22 16:02:24 -0400

    adding MyNameIs

---
+++ src/main/java/MyNameIs.java
@@ -0,0 +1,7 @@
+public class MyNameIs {
+    public static void main(String[] args) {
+        System.out.println("Hi name is");
+        System.out.println("Gabriela");
+        System.out.println("Erazo");
+    }
+}
